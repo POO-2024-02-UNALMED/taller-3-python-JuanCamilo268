8766d5a739d97f77eebb6bf77ba39e0a938104eb
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-16 16:43:06 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -6,7 +6,7 @@ class TV:
         self._canal = 1
         self._volumen = 1
         self._precio = 500
-        self.control = ""
+        self.control = None
         TV._numTV += 1
     def setMarca(self, marca):
         self._marca = marca
