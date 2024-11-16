206a3a9242a817ccbc507b0f825d6011529cc74f
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-16 16:10:16 -0500

    Update marca.py

--- televisores/marca.py
+++ televisores/marca.py
@@ -1,7 +1,9 @@
 class Marca:
-    def __init__(self,nombre):
-        self.nombre = nombre
-    def setNombre(nombre):
-        self.nombre = nombre
-    def getNombre():
-        return self.nombre
+    def __init__(self,_nombre):
+        self._nombre = _nombre
+   
+    def setNombre(self, nombre):
+        self._nombre = nombre
+   
+    def getNombre(self):
+        return self._nombre
