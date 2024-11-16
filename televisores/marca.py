0babe2295b58499621c13d76eae7434a7e16c0b8
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-16 14:30:02 -0500

    Update marca.py

---
+++ televisores/marca.py
@@ -0,0 +1,7 @@
+class Marca:
+    def __init__(self,nombre):
+        self.nombre = nombre
+    def setNombre(nombre):
+        self.nombre = nombre
+    def getNombre():
+        return self.nombre
